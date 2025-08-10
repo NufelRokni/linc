8b2051b6110425f1e7f1a71e77b205d6612d939e
NufelRokni <nufelrokni675@gmail.com>
2025-08-10 21:21:23 +0100

    Update run_expts.sh

--- run_expts.sh
+++ run_expts.sh
@@ -5,22 +5,13 @@ set -e
 outdir="outputs"
 mkdir -p ${outdir}
 
-# for model in "bigcode/starcoderplus"; do
 for model in "mistralai/Mistral-7B-v0.1"; do
     # for base in "folio" "proofwriter"; do
     for base in "folio"; do
-        if [[ ${model} == "bigcode/starcoderplus" ]]; then
-            batch_size=1
-            max_length=8192 # max model context including prompt
-            precision="bf16"
         elif [[ ${model} == "mistralai/Mistral-7B-v0.1" ]]; then
             batch_size=1
             max_length=8192 # max model context including prompt
             precision="fp32"
-        else
-            echo "Unknown model ${model}"
-            exit 1
-        fi
         # for n in "1" "2" "4" "8"; do
         for n in "8"; do
             for mode in "baseline" "scratchpad" "cot" "neurosymbolic"; do
@@ -28,6 +19,7 @@ for model in "mistralai/Mistral-7B-v0.1"; do
                 task="${base}-${mode}-${n}shot"
                 run_id="${model#*/}_${task}"
                 job="cd $(pwd); source activate linc; "
+                
                 if [[ ! -z "${DEBUG}" ]]; then
                     job+="accelerate launch --module debugpy --listen 0.0.0.0:5679 --wait-for-client runner.py"
                 else
